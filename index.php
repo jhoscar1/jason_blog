2bdb7e9fec9c92b4607592eb4d0417d7d07fe1e4
Jason Oscar <jhoscar@gmail.com>
2014-07-25 17:10:17 +0000

    Day 5 Exercises

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="index.css">
+<title>Dev Camp Blog</title>
+	<body>
+		<h1>Jason's Dev Blog</h1>
+
+<?php 
+	$connection = mysql_connect('127.0.0.1', 'root', '');
+	mysql_select_db('jason_blog');
+
+	if (!$connection) {
+  die('Unable to connect: ' . mysql_errno());
+}
+
+	if($connection){
+		$result = mysql_query('SELECT title, author, body, id FROM Articles ORDER BY id DESC');
+
+		while($page=mysql_fetch_array($result)){
+			print '<div>';
+			print '<h2><a href="article.php?id=' . $page['id'] . '">' . $page['title'] . '</a></h2>';
+			print '<h4>' . 'by ' . $page['author'] . '</h4>';
+			print '</div>';
+
+		}
+	}
+
+mysql_close($connection);
+
+
+
+
+
+?>
+	</body>
+</html>
